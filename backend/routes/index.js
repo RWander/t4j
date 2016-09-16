5e1bb59fc79da61d40f264e0f95770faf59fd650
Roman Korneev <roman.korneyev@gmail.com>
2016-09-16 12:41:05 +0400

    actions

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,14 @@ module.exports = function (app) {
     res.json({data: Date.now()});
   });
 
+  app.get('/data', function(req, res) {
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+      data[i] = Date.now() + i;
+    }
+    res.json({data:data});
+  });
+
   fs.readdirSync(__dirname).forEach(function(file) {
     if (file !== 'index.js') {
       let name = file.substr(0, file.indexOf('.'));
