ae618a6be8f86dd34d61c989c1565cb538555f9c
Roman Korneev <roman.korneyev@gmail.com>
2016-09-22 01:45:41 +0400

    refactoring

--- frontend/indexRender.js
+++ frontend/indexRender.js
@@ -16,6 +16,16 @@ const env = process.env.NODE_ENV;
 const config = require(`./config/${env ? env : 'development'}`);
 const backendUrl = config.BACKEND.replace(/"/g,'');
 
+module.exports.getInitData = function(next) {
+  _backendGET(
+    'data',
+    // success
+    res => next(null, res.data),
+    // fail
+    err => next(err)
+  );
+};
+
 module.exports.returnResponse = function (req, res, data) {
   // Create a new Redux store instance
   const memoryHistory = createMemoryHistory(req.url);
@@ -33,55 +43,49 @@ module.exports.returnResponse = function (req, res, data) {
     } else if (redirectLocation) {
       res.redirect(302, redirectLocation.pathname + redirectLocation.search);
     } else if (renderProps) {
-      // Render the component to a string
-      const html = renderToString(
-        React.createElement(
-          Provider,
-          { store: store },
-          React.createElement(
-            'div',
-            null,
-            React.createElement(RouterContext, renderProps),
-            process.env.NODE_ENV !== 'production' ? React.createElement(DevTools, null) : null
-          )
-        )
-      );
-
-      // Grab the initial state from our Redux store
-      const preloadedState = store.getState();
-
-      // Send the rendered page back to the client
-      res.setHeader('Content-Type', 'text/html; charset=utf-8');
-      res.send(`
-        <!doctype html>
-        <html>
-          <head>
-            <title>Redux Universal Example</title>
-          </head>
-          <body>
-            <div id="root">${html}</div>
-            <script>
-              window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)}
-            </script>
-            <script src="/main.js"></script>
-          </body>
-        </html>`
-      );
+      _renderHtml(res, store, renderProps);
     } else {
       res.status(404).send('Not Found');
     }
   });
 };
 
-module.exports.getInitData = function(next) {
-  _backendGET(
-    'data',
-    // success
-    res => next(null, res.data),
-    // fail
-    err => next(err)
+function _renderHtml(res, store, renderProps) {
+  // Render the component to a string
+  const html = renderToString(
+    React.createElement(
+      Provider,
+      { store: store },
+      React.createElement(
+        'div',
+        null,
+        React.createElement(RouterContext, renderProps),
+        process.env.NODE_ENV !== 'production' ? React.createElement(DevTools, null) : null
+      )
+    )
   );
-};
+
+  // Grab the initial state from our Redux store
+  const preloadedState = store.getState();
+
+  // Send the rendered page back to the client
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.send(`
+    <!doctype html>
+    <html>
+      <head>
+        <title>Redux Universal Example</title>
+      </head>
+      <body>
+        <div id="root">${html}</div>
+        <script>
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)}
+        </script>
+        <script src="/main.js"></script>
+      </body>
+    </html>`
+  );
+}
 
 function _backendGET(serverMethod, success, fail) {
   const url = `${backendUrl}/${serverMethod}`;
